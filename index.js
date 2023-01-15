749153a9e8f4bb6fac07649c5353d1835c86a350
Spencer Pingitore <silentfoe6@gmail.com>
2023-01-15 01:34:43 -0500

    trouble shooting js functions

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+
+document.querySelector("#increment-btn").addEventListener("click",increment)
+document.querySelector("#save-btn").addEventListener("click",save)
+
 function increment() {
     count += 1
     countEl.textContent = count
